0fd6be57bf4b44b39fa91c9a476a74555a483d7a
Zhengyao Lin <zhengyao-lin@outlook.com>
2025-10-10 11:55:41 -0400

    Some drafts

--- Wavelet/Determinancy.lean
+++ Wavelet/Determinancy.lean
@@ -61,17 +61,23 @@ class WithToken V where
 State invariant on Seq: for any two values v₁ v₂ in the config, token v₁ ⊥ token v₂
 -/
 
-inductive GuardedLts {S} (Inv : S → Prop) (lts : Lts S E) : Lts S E where
-  | step : Inv s → Inv s' → lts.Step s e s' → GuardedLts Inv lts s e s'
+/-- Restricts an LTS by imposing a state and label invariant. -/
+inductive GuardedLts {S} (InvS : S → Prop) (InvE : E → Prop) (lts : Lts S E) : Lts S E where
+  | step :
+    InvS s → InvE e → InvS s' →
+    lts.Step s e s' →
+    GuardedLts InvS InvE lts s e s'
 
 /-- Guard the transition of a semantics with the given invariant. -/
 def guard [Arity Op]
   (sem : Semantics Op V m n)
-  (Inv : sem.S → Prop) : Semantics Op V m n :=
+  (InvS : sem.S → Prop)
+  (InvE : Label Op V m n → Prop) :
+  Semantics Op V m n :=
   {
     S := sem.S,
     init := sem.init,
-    lts := GuardedLts Inv sem.lts,
+    lts := GuardedLts InvS InvE sem.lts,
     -- TODO: this is actually not true,
     -- maybe remove this requirement?
     yields_functional := sorry
@@ -125,6 +131,55 @@ instance instInterpConstsSum [left : InterpConsts V] : InterpConsts (V ⊕ V') w
       simp [this]
     · contradiction
 
+/-- PCM specification of an operator set -/
+structure OpSpec' Op V T [Arity Op] [PCM T] where
+  requires : (op : Op) → Vector V (Arity.ι op) → T
+  ensures : (op : Op) → Vector V (Arity.ι op) → T
+  frame_preserving : ∀ op inputs, requires op inputs ⟹ ensures op inputs
+
+def WithSpec Op [Arity Op] [PCM T] (_spec : OpSpec' Op V T) : Type u := Op
+
+/-- Adds an additional argument for ghost permissions. -/
+instance instArityWithSpec
+  [arity : Arity Op] [PCM T]
+  {spec : OpSpec' Op V T} :
+  Arity (WithSpec Op spec) where
+  ι op := arity.ι op + 1
+  ω op := arity.ω op + 1
+
+instance OpInterp.withSpec
+  [Arity Op] [inst : OpInterp Op V] [PCM T]
+  (spec : OpSpec' Op V T) :
+  OpInterp (WithSpec Op spec) (V ⊕ T) where
+  S := inst.S
+  init := inst.init
+  /-
+  Checks the ghost tokens against the spec,
+  then pass through the inputs/outputs to
+  the base interpretation.
+  -/
+  interp op inputs state outputs s' :=
+    ∃ tok₁ tok₂ inputs' outputs',
+      inputs.back = .inr tok₁ ∧
+      outputs.back = .inr tok₂ ∧
+      inputs'.map .inl = inputs.pop ∧
+      outputs'.map .inl = outputs.pop ∧
+      tok₁ ≡ spec.requires op inputs' ∧
+      tok₂ ≡ spec.ensures op inputs' ∧
+      inst.interp op inputs' state outputs' s'
+
+/-- Stucks if a ghost token is received as opposed to an actual value. -/
+instance OpInterp.withGhost
+  [Arity Op] [inst : OpInterp Op V] :
+  OpInterp Op (V ⊕ T) where
+  S := inst.S
+  init := inst.init
+  interp op inputs state outputs s' :=
+    ∃ inputs' outputs',
+      inputs'.map .inl = inputs ∧
+      outputs'.map .inl = outputs ∧
+      inst.interp op inputs' state outputs' s'
+
 end Wavelet.Semantics
 
 namespace Wavelet.Seq
@@ -139,6 +194,28 @@ def Config.DisjointTokens
     c.vars.getVar x₁ = some (.inr t₁) →
     c.vars.getVar x₂ = some (.inr t₂) →
     t₁ ⊥ t₂
+
+inductive Config.YieldMeetsSpec
+  [arity : Arity Op] [PCM T]
+  {spec : OpSpec' Op V T} :
+  Label (WithSpec Op spec) (V ⊕ T) m n → Prop where
+  -- Reads the last ghost token and checks it against the spec
+  | spec_yield
+    {inputs : Vector (V ⊕ T) (arity.ι op + 1)}
+    {outputs : Vector (V ⊕ T) (arity.ω op + 1)}
+    {inputs' : Vector V (arity.ι op)}
+    {outputs' : Vector V (arity.ω op)} :
+    inputs.back = .inr tok₁ →
+    outputs.back = .inr tok₂ →
+    inputs'.map .inl = inputs.pop →
+    outputs'.map .inl = outputs.pop →
+    tok₁ ≡ spec.requires op inputs' →
+    tok₂ ≡ spec.ensures op inputs' →
+    YieldMeetsSpec (.yield op inputs outputs)
+  | spec_input :
+    YieldMeetsSpec (.input inputs)
+  | spec_output :
+    YieldMeetsSpec (.output outputs)
 
 end Wavelet.Seq
 
@@ -171,25 +248,43 @@ theorem sim_compile_fn'
   [InterpConsts V]
   [PCM T]
   [DecidableEq χ]
-  (fn : Fn Op χ (V ⊕ T) m n)
+  {spec : OpSpec' Op V T}
+  (fn : Fn (WithSpec Op spec) χ (V ⊕ T) m n)
   (hnz : m > 0 ∧ n > 0)
   (hwf : fn.AffineVar) :
-  fn.semantics.guard (Config.DisjointTokens)
-    ≲ᵣ (compileFn hnz fn).semantics.guard (Config.DisjointTokens)
+  fn.semantics.guard Config.DisjointTokens (λ _ => True)
+    ≲ᵣ (compileFn hnz fn).semantics.guard Config.DisjointTokens (λ _ => True)
   := sorry
 
--- theorem fn_progress
---   [Arity Op]
---   [InterpConsts V]
---   [WithToken V T]
---   [PCM T]
---   [DecidableEq χ]
---   (fn : Fn Op χ V m n)
---   (hnz : m > 0 ∧ n > 0)
---   (hwf : fn.AffineVar)
---   (hwell_typed : sorry) :
---   fn.semantics -- Maybe even add the tokens here?
---     ≲ᵣ fn.semantics.guard (Config.DisjointTokens T)
---   := sorry
+def eraseGhost
+  [Arity Op]
+  [PCM T]
+  {spec : OpSpec' Op V T}
+  (proc : Proc (WithSpec Op spec) χ (V ⊕ T) m n) : Proc Op χ V m n
+  := sorry
+
+def attachToken
+  [Arity Op]
+  (fn : Fn Op χ V m n) : Fn Op χ (V ⊕ T) m n := {
+    params := fn.params
+    body := fn.body
+  }
+
+theorem fn_progress
+  [Arity Op]
+  [InterpConsts V]
+  [PCM T]
+  [DecidableEq χ]
+  {spec : OpSpec' Op V T}
+  (fn : Fn Op χ V m n)
+  {fn' : Fn (WithSpec Op spec) χ (V ⊕ T) m n}
+  (hnz : m > 0 ∧ n > 0)
+  (hwf : fn.AffineVar)
+  (hwt : sorry) -- Well-typedness: generates the elaborated `fn'` from `fn`
+  (interp : OpInterp Op V) :
+  (attachToken fn).semantics.interpret interp.withGhost
+    ≲ᵣ (fn'.semantics.guard Config.DisjointTokens (λ _ => True)).interpret
+        (interp.withSpec spec)
+  := sorry
 
 end Wavelet.Compile
