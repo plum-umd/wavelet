1d4df22d2eb27e9e00bb692d1dbd6e2e6fd24bce
Zhengyao Lin <zhengyao-lin@outlook.com>
2025-11-19 16:54:14 -0500

    Add basic CI

--- lean/lakefile.lean
+++ lean/lakefile.lean
@@ -11,3 +11,21 @@ lean_lib Wavelet
 @[default_target]
 lean_exe wavelet where
   root := `Main
+  moreLinkArgs :=
+    if System.Platform.isOSX then
+      #[
+        "-dead_strip",
+        "-dead_strip_dylibs",
+        "-Wl,--icf=all",
+        "-Wl,-x",
+        "-Wl,-S",
+      ]
+    else if System.Platform.isWindows then
+      #[]
+    else
+      #[
+        "-Wl,--gc-sections",
+        "-Wl,--icf=all",
+        "-Wl,--strip-all",
+        "-Wl,--strip-debug",
+      ]
