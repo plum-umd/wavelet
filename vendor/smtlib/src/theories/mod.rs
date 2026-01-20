fe803ab4b260005f895ddf331e267773a21a070b
Zhengyao Lin <zhengyao-lin@outlook.com>
2026-01-20 11:28:43 +0800

    Move vendored smtlib-rs to a separate repo

--- vendor/smtlib/src/theories/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Theories in SMT-LIB are definitions of [sorts](crate::sorts::Sort) and in
-//! general functions present in the [logics](crate::Logic).
-
-pub mod arrays_ex;
-pub mod core;
-pub mod fixed_size_bit_vectors;
-pub mod ints;
-pub mod reals;
