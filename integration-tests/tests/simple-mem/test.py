4ec5c4662c210ed892beddb6c76dc81feb8f48c5
Zhengyao Lin <zhengyao-lin@outlook.com>
2026-02-03 13:43:50 +0800

    Tweak simple-mem integration test to test internal memory

--- integration-tests/tests/simple-mem/test.py
+++ integration-tests/tests/simple-mem/test.py
@@ -5,5 +5,6 @@ from .. import helper
 async def test(dut):
     hs_dut = helper.HandshakeDut(dut)
     await hs_dut.init()
-    await hs_dut.assert_io([0, 5], [None], expected_memory={ "res": [sum(range(6))] })
-    await hs_dut.assert_io([0, 50], [None], expected_memory={ "res": [sum(range(51))] })
+    await hs_dut.assert_io([0, 0], [0])
+    await hs_dut.assert_io([0, 5], [sum(range(6))])
+    await hs_dut.assert_io([0, 50], [sum(range(51))])
--- integration-tests/tests/simple-mem/test.rs
+++ integration-tests/tests/simple-mem/test.rs
@@ -1,5 +1,5 @@
 #[cap(res: uniq @ 0..1)]
-pub fn sum(a: u32, b: u32, res: &mut [u32; 1]) {
+pub fn sum_aux(a: u32, b: u32, res: &mut [u32; 1]) -> u32 {
     let cond = a <= b;
     if cond {
         let s = res[0];
@@ -8,8 +8,17 @@ pub fn sum(a: u32, b: u32, res: &mut [u32; 1]) {
         res[0] = new_s;
         let new_a = a + 1;
         fence!();
-        sum(new_a, b, res)
+        sum_aux(new_a, b, res)
     } else {
-        ()
+        let r = res[0];
+        r
     }
+}
+
+#[cap(res: uniq @ 0..1)]
+pub fn sum(a: u32, b: u32, #[alloc] res: &mut [u32; 1]) -> u32 {
+    let z = 0u32;
+    res[0] = z;
+    fence!();
+    sum_aux(a, b, res)
 }
