4a6f98ce337e79d3f68371b33f17ddc20ca425e4
Zhengyao Lin <zhengyao-lin@outlook.com>
2026-02-03 13:05:07 +0800

    Add integration test nn_conv

---
+++ integration-tests/tests/nn_conv/test.py
@@ -0,0 +1,101 @@
+import cocotb
+from .. import helper
+from .. import matrix
+
+def nn_conv(SRC, W, OUT,
+            weight, src,
+            weight_rows, weight_cols,
+            wc_bump, wc_wr_bump,
+            shift):
+    dest = [0] * OUT
+
+    def clamp_i16(w):
+        if w < -32768:
+            return -32768
+        elif w > 32767:
+            return 32767
+        else:
+            return w
+
+    def dot_window_aux(j, row, col, src_idx, base, acc):
+        if j < W:
+            a = weight[j]
+            src_offset = base + src_idx
+            if src_offset < SRC:
+                b = src[src_offset]
+                prod = a * b
+                sum_ = acc + prod
+
+                col1 = col + 1
+                src_idx1 = src_idx + 1
+                j1 = j + 1
+
+                hit_col_end = (col1 == weight_cols)
+                if hit_col_end:
+                    col2 = 0
+                    row1 = row + 1
+                    src_idx2 = src_idx1 + wc_bump
+
+                    hit_row_end = (row1 == weight_rows)
+                    if hit_row_end:
+                        row2 = 0
+                        src_idx3 = src_idx2 + wc_wr_bump
+                        return dot_window_aux(
+                            j1, row2, col2, src_idx3, base, sum_
+                        )
+                    else:
+                        return dot_window_aux(
+                            j1, row1, col2, src_idx2, base, sum_
+                        )
+                else:
+                    return dot_window_aux(
+                        j1, row, col1, src_idx1, base, sum_
+                    )
+            else:
+                return acc
+        else:
+            return acc
+
+    def nn_conv_aux(i):
+        if i < OUT:
+            w_raw = dot_window_aux(
+                0, 0, 0, 0, i, 0
+            )
+
+            shifted = w_raw >> shift
+            clipped = clamp_i16(shifted)
+            dest[i] = clipped
+
+            nn_conv_aux(i + 1)
+
+    nn_conv_aux(0)
+    return dest
+
+@cocotb.test()
+async def test(dut):
+    hs_dut = helper.HandshakeDut(dut)
+    await hs_dut.init()
+
+    for SRC, W, OUT, weight_rows, weight_cols, wc_bump, wc_wr_bump, shift in [
+        (16, 4, 13, 2, 2, 2, 1, 0),
+    ]:
+        print(f"testing nn_conv (src = {SRC}, w = {W}, out = {OUT}, weight_rows = {weight_rows}, weight_cols = {weight_cols}, wc_bump = {wc_bump}, wc_wr_bump = {wc_wr_bump}, shift = {shift})")
+        src = matrix.random_matrix(1, SRC, lower=0, upper=300)
+        weight = matrix.random_matrix(1, W, lower=-10, upper=10)
+        await hs_dut.assert_io(
+            [weight_rows, weight_cols, wc_bump, wc_wr_bump, shift, SRC, W, OUT],
+            [None],
+            init_memory={
+                "src": sum(src, []),
+                "weight": sum(weight, []),
+            },
+            expected_memory={
+                "dest": nn_conv(
+                    SRC, W, OUT,
+                    sum(weight, []), sum(src, []),
+                    weight_rows, weight_cols,
+                    wc_bump, wc_wr_bump,
+                    shift
+                ),
+            },
+        )
