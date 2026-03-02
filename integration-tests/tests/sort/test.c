da4d15fb7c0f12974922acac929214daf626a76c
Zhengyao Lin <zhengyao-lin@outlook.com>
2026-03-02 14:21:42 +0800

    Fix the potential memory issue in the `sort` integration test

--- integration-tests/tests/sort/test.c
+++ integration-tests/tests/sort/test.c
@@ -1,30 +1,22 @@
 void sort(int A[1024], int Z[1024], int even_count, int size) {
 	int count = even_count;
-	
 	for(int i = 0; i < 32; i++) {
 		int odd = i & 0x1;
+		int * src = (odd) ? Z : A;
+		int * dst = (odd) ? A : Z;
+
 		int idx0 = 0;
 		int idx1 = count;
 		int next_count = 0;
 
 		for(int j = 0; j < size; j++) {
-			int v = odd ? Z[j] : A[j];
+			int v = src[j];
 			int o = (v >> i) & 0x1;
 			next_count += (v & (1 << (i + 1))) == 0x0;
 
-			if (o) {
-				if (odd) {
-					A[idx1++] = v;
-				} else {
-					Z[idx1++] = v;
-				}
-			} else {
-				if (odd) {
-					A[idx0++] = v;
-				} else {
-					Z[idx0++] = v;
-				}
-			}
+			int idx = (o) ? idx1++ : idx0++;
+
+			if (idx < size) dst[idx] = v;
 		}
 
     	count = next_count;
