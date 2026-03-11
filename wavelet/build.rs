46c7d46a7da0c753b503054b9fffe26527a8f6f1
Zhengyao Lin <zhengyao-lin@outlook.com>
2026-03-11 20:50:21 +0800

    Fix linking again

--- wavelet/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    #[cfg(target_os = "linux")]
-    {
-        // Fixes a linking issue on Linux with GNU ld.
-        println!("cargo::rustc-link-arg=-Wl,-Bstatic");
-        println!("cargo::rustc-link-arg=-lleanrt");
-    }
-}
